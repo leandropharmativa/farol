7a247ad7ca07a678427f0be326922eae41e67e9f
leandropharmativa <leandro@pharmativa.com.br>
2025-06-12 11:38:41 -0300

    Create models.py

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+# backend/models.py
+
+from pydantic import BaseModel
+from typing import Optional
+
+class SerialRequest(BaseModel):
+    nomeEmpresa: str
+    email: str
+    validadeDias: Optional[int] = 30
+
+class ValidarSerialRequest(BaseModel):
+    codigo: str
+    email: str
