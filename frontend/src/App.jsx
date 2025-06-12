20dec861d913bf4f01a2668eda38bc1dac2539f8
leandropharmativa <leandro@pharmativa.com.br>
2025-06-12 14:44:48 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-
 import React from 'react'
 
 import LoginAdmin from './pages/LoginAdmin'
 import GerarSerial from './pages/GerarSerial'
 import LoginFarmacia from './pages/LoginFarmacia'
 import AtivarContaFarmacia from './pages/AtivarContaFarmacia'
+import PainelFarmacia from './pages/PainelFarmacia' // Adicione este arquivo com tela inicial pós-login
 import { getToken } from './utils/auth'
 
 export default function App() {
@@ -13,17 +13,25 @@ export default function App() {
 
   return (
     <BrowserRouter>
-<Routes>
-<Route path="/farmacia" element={<LoginFarmacia />} />
- 
-  <Route path="/" element={<LoginAdmin />} />
-  <Route path="/login" element={<LoginAdmin />} />
-  <Route
-    path="/gerar"
-    element={token ? <GerarSerial /> : <Navigate to="/" />}
-  />
-</Routes>
+      <Routes>
+        {/* Página inicial padrão: login da farmácia */}
+        <Route path="/" element={<LoginFarmacia />} />
 
+        {/* Ativação da conta da farmácia (primeiro acesso) */}
+        <Route path="/ativar/:codigo" element={<AtivarContaFarmacia />} />
+
+        {/* Painel da farmácia após login */}
+        <Route path="/painel-farmacia" element={<PainelFarmacia />} />
+
+        {/* Login do admin - acesso manual */}
+        <Route path="/login" element={<LoginAdmin />} />
+
+        {/* Geração de serial (rota protegida) */}
+        <Route
+          path="/gerar"
+          element={token ? <GerarSerial /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </BrowserRouter>
   )
 }
