b02666eda9979059bf5685a5b3cf9229980e98ea
leandropharmativa <leandro@pharmativa.com.br>
2025-06-12 12:08:44 -0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LoginAdmin from './pages/LoginAdmin'
+import GerarSerial from './pages/GerarSerial'
+import { getToken } from './utils/auth'
+
+export default function App() {
+  const token = getToken()
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginAdmin />} />
+        <Route
+          path="/gerar"
+          element={token ? <GerarSerial /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </BrowserRouter>
+  )
+}
