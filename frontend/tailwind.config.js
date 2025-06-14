4bc5a751640743ed92bbc97534b846629d8113de
leandropharmativa <leandro@pharmativa.com.br>
2025-06-14 18:03:52 -0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,22 @@
-theme: {
-  extend: {
-    colors: {
-      farol: {
-        primary: '#750c0d',
-        primaryfocus: '#de3c3d',
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.css",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        farol: {
+          primary: '#750c0d',
+          primaryfocus: '#de3c3d',
+        },
+      },
+      fontFamily: {
+        sans: ['Lato', 'sans-serif'],
       },
     },
-    fontFamily: {
-      sans: ['Lato', 'sans-serif'],
-    },
   },
-},
+  plugins: [],
+}
