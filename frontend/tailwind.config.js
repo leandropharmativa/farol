2bdfc129065a3052810dba2f86831cb589ff51ff
leandropharmativa <leandro@pharmativa.com.br>
2025-06-15 13:40:31 -0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,28 +5,37 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.css",
   ],
+  safelist: [
+    'bg-farol-loc1',
+    'bg-farol-loc2',
+    'bg-farol-loc3',
+    'bg-farol-loc4',
+    'bg-farol-loc5',
+    'bg-farol-loc6',
+    'text-white', // necessário se não houver uso estático
+  ],
   theme: {
     extend: {
       colors: {
         farol: {
-          primary: '#d4674c', //laranja
-          secondary:'#67727e', //cinza
-          focus: '#dde5e7', //cinza claro
-	  linecolor: '#1791b1', //azul
-	  solidos:'#d5a6bd',
+          primary: '#d4674c',
+          secondary:'#67727e',
+          focus: '#dde5e7',
+          linecolor: '#1791b1',
+          solidos:'#d5a6bd',
           semisolidos:'#d9d2e9',
-	  saches:'#f6b26b',
-	  loc1:'#980000',
-	  loc2:'#ffc8aa',
+          saches:'#f6b26b',
+          loc1:'#980000',
+          loc2:'#ffc8aa',
           loc3:'#bfe1f6',
-	  loc4:'#ffe5a0',
-	  loc5:'#e6cff2',
-	  loc6:'#d4edbc',
+          loc4:'#ffe5a0',
+          loc5:'#e6cff2',
+          loc6:'#d4edbc',
         },
       },
       fontFamily: {
         sans: ['Lato', 'sans-serif'],
-	pacifico: ['Pacifico', 'cursive'],
+        pacifico: ['Pacifico', 'cursive'],
       },
     },
   },
