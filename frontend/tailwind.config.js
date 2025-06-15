d60f5aafe86d028d96fd6804df9f821fccdf5d83
leandropharmativa <leandro@pharmativa.com.br>
2025-06-15 13:21:21 -0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
 	  linecolor: '#1791b1', //azul
 	  solidos:'#d5a6bd',
           semisolidos:'#d9d2e9',
+	  saches:'#f6b26b',
 	  loc1:'#980000',
 	  loc2:'#ffc8aa',
           loc3:'#bfe1f6',
