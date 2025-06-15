67fcd534655e6a4dad4d6b53e72611d7f35270bc
leandropharmativa <leandro@pharmativa.com.br>
2025-06-15 10:05:58 -0300

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -2,9 +2,14 @@
 
 import os
 import psycopg2
+import psycopg2.extras
+import uuid
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# ✅ Garante que UUID do Python seja aceito automaticamente pelo psycopg2
+psycopg2.extras.register_uuid()
 
 conn = psycopg2.connect(os.getenv("DATABASE_URL"))
 conn.autocommit = True
