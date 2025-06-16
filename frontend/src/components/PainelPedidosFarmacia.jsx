3d80baa9178c7f797c53abd87f11d5816ff161c2
leandropharmativa <leandro@pharmativa.com.br>
2025-06-16 02:55:08 -0300

    Update PainelPedidosFarmacia.jsx

--- frontend/src/components/PainelPedidosFarmacia.jsx
+++ frontend/src/components/PainelPedidosFarmacia.jsx
@@ -128,7 +128,11 @@ const coresDisponiveis = [
   'bg-farol-loc3 text-white',
   'bg-farol-loc4 text-white',
   'bg-farol-loc5 text-white',
-  'bg-farol-loc6 text-white'
+  'bg-farol-loc6 text-white',
+  'bg-farol-loc7 text-white',
+  'bg-farol-loc8 text-white',
+  'bg-farol-loc9 text-white',
+  'bg-farol-loc10 text-white'
 ]
 let indiceCorAtual = 0
 
