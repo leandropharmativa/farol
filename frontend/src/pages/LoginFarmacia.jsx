1636193061ba5236ea28dd9e931055ce36d642c4
leandropharmativa <leandro@pharmativa.com.br>
2025-06-17 19:45:28 -0300

    Update LoginFarmacia.jsx

--- frontend/src/pages/LoginFarmacia.jsx
+++ frontend/src/pages/LoginFarmacia.jsx
@@ -149,7 +149,7 @@ export default function LoginFarmacia() {
         </div>
 
         {/* Nome Farol no topo centralizado */}
-      <div className="left-1/2 -translate-x-1/2 justify-center text-3xl text-white fonte-pacifico">
+      <div className="absolute top-4 left-1/2 -translate-x-1/2 text-3xl font-bold text-farol-primary fonte-pacifico text-center">
         Farol
       </div>
 
