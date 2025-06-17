a00fceacb111dd71fb2ab5b44005cc83f26eaf7f
leandropharmativa <leandro@pharmativa.com.br>
2025-06-17 00:49:30 -0300

    Update ModalConfirmacao.jsx

--- frontend/src/components/ModalConfirmacao.jsx
+++ frontend/src/components/ModalConfirmacao.jsx
@@ -10,9 +10,9 @@ export default function ModalConfirmacao({ titulo, onConfirmar, onCancelar, coor
 
   const inputRef = useRef(null)
 
-useEffect(() => {
-  if (inputRef.current) inputRef.current.focus()
-}, [])
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus()
+  }, [])
 
   const isConferencia = titulo?.toLowerCase().includes('conferência')
 
@@ -32,28 +32,26 @@ useEffect(() => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 z-50">
-  <div
-    className="absolute bg-white w-full max-w-xs p-4 rounded-lg shadow-md animate-fadeIn max-h-screen overflow-y-auto"
-    style={{
-      top: coordenadas?.top ?? '50%',
-      left: coordenadas?.left ?? '50%',
-      transform: coordenadas ? 'translateY(0)' : 'translate(-50%, -50%)'
-    }}
-  >
-      <div className="bg-white w-full max-w-xs p-4 rounded-lg shadow-md animate-fadeIn max-h-screen overflow-y-auto">
+      <div
+        className="absolute bg-white w-full max-w-xs p-4 rounded-lg shadow-md animate-fadeIn max-h-screen overflow-y-auto"
+        style={{
+          top: coordenadas?.top ?? '50%',
+          left: coordenadas?.left ?? '50%',
+          transform: coordenadas ? 'translateY(0)' : 'translate(-50%, -50%)'
+        }}
+      >
         <h2 className="text-base font-semibold text-left text-farol-primary mb-2">
           Confirmar {titulo.replace('etapa ', '').replace(/"/g, '')}
         </h2>
 
-<input
-  ref={inputRef}
-  type="text"
-  placeholder="Código do Usuário"
-  className="modal-confirmacao-input rounded-full"
-  value={codigo}
-  onChange={(e) => setCodigo(e.target.value)}
-/>
-
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Código do Usuário"
+          className="modal-confirmacao-input rounded-full"
+          value={codigo}
+          onChange={(e) => setCodigo(e.target.value)}
+        />
 
         <input
           type="text"
